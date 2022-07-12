b7d0048e2476d64f8bf58a7faa0189397a67af65
Matej Hajnal <matej.hajnal@gmail.com>
2022-07-12 17:50:22 +0200

    documentation added

--- src/parse.py
+++ src/parse.py
@@ -3,28 +3,55 @@ import math
 
 
 def dummy_colision_finder(csvfile, size):
+    """ Parses  a loopy csv file nn/ai. It prints the frame numbers where the additional agents. The print includes agent's id.
+        Returns a list of frames where an additional agent was found
+
+    :param csvfile: input file
+    :param size: expected number of agents
+    :return frame_numbers_of_collided_agents: list of frames where an additional agent was found
+    """
     reader = csv.DictReader(csvfile)
     i = 0
+    frame_numbers_of_collided_agents = []
+
     for row in reader:
         # print(row['oid'])
         if int(row['oid']) > size - 1:
             print("A new fake agents appears on frame number", row['frame_number'], "iteration number", i, "with oid", row['oid'])
+            frame_numbers_of_collided_agents.append(row['frame_number'])
             size = size + 1
         i = i + 1
 
+    return frame_numbers_of_collided_agents
+
 
 def parse_traces(csvfile):
+    """ Parses a loopy csv file nn/ai and returns a dictionary of traces 'oid' -> 'frame_number' -> location [x,y]
+
+    :arg
+        csvfile (file): input file
+
+    :returns
+        traces (dic): dictionary of traces 'oid' -> 'frame_number' -> location [x,y]
+    """
     traces = dict()
     reader = csv.DictReader(csvfile)
     for row in reader:
         if int(row['oid']) not in traces.keys():
-            print("hello", row['oid'])
+            # print("hello", row['oid'])
             traces[int(row['oid'])] = dict()
         traces[int(row['oid'])][row['frame_number']] = [row['x'], row['y']]
     return traces
 
 
 def trace_len(trace: dict):
+    """ Parses a trace - see traces in fun parse_traces. Returns number of frames of the trace, [minimal, maximal frame number], length of the trace in x,y space.
+
+    :param trace: a single trace of traces
+
+    :returns [number_of_frames, frame_range_len, trace_lenn]: [number of frames of the trace, [minimal, maximal frame number], length of the trace in x,y space]
+
+    """
     frames = sorted(list(map(float, trace.keys())))
 
     number_of_frames = len(trace.keys())
@@ -39,7 +66,9 @@ def trace_len(trace: dict):
         try:
             trace_lenn = trace_lenn + math.dist(list(map(float, (trace[frames[index]]))), list(map(float, (trace[frames[index+1]]))))
         except Exception as err:
-            print(err)
+            print(trace)
+            print("Error:", str(err))
+            raise err
 
     return number_of_frames, frame_range_len, trace_lenn
 
@@ -55,12 +84,13 @@ if __name__ == "__main__":
             # print(row['oid'])
 
     with open('../data/Video_tracking/190822/20190822_112842909_2BEE_generated_20210503_074806_nn.csv', newline='') as csvfile:
-        dummy_colision_finder(csvfile, 2)
+        # dummy_colision_finder(csvfile, 2)
+        pass
 
     with open('../data/Video_tracking/190822/20190822_112842909_2BEE_generated_20210503_074806_nn.csv', newline='') as csvfile:
         traces = parse_traces(csvfile)
-        print()
-        print(traces[2])
+        # print()
+        # print(traces[2])
         print(trace_len(traces[1]))
 
     # print(i)
