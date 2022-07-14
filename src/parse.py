d1b14770318aba4b3f64eac425caf5fc2ed2ba49
Matej Hajnal <matej.hajnal@gmail.com>
2022-07-14 19:57:54 +0200

    CROSS-TRACE ANALYSIS

--- src/parse.py
+++ src/parse.py
@@ -1,6 +1,6 @@
 import csv
 import math
-
+from termcolor import colored
 
 class Trace:
     """ Single agent trace obtained from the loopy csv file or changed within the analysis.
@@ -98,7 +98,7 @@ def trace_len(trace: dict):
                 # print("Error:", str(err))
                 raise err
 
-    return number_of_frames, frame_range_len, trace_lenn, max_step_len, max_step_len_step_index, max_step_len_line, max_step_len_frame_number
+    return frame_range, number_of_frames, frame_range_len, trace_lenn, max_step_len, max_step_len_step_index, max_step_len_line, max_step_len_frame_number
 
 
 if __name__ == "__main__":
@@ -121,21 +121,43 @@ if __name__ == "__main__":
     with open('../data/Video_tracking/190822/test.csv', newline='') as csvfile:
         traces = parse_traces(csvfile)
         # print()
-        # print(traces[2])
+        print(traces[0])
         print("number_of_frames, frame_range_len, trace_length:", trace_len(traces[0]))
+        print(traces[1])
         print("number_of_frames, frame_range_len, trace_length:", trace_len(traces[1]))
 
     with open('../data/Video_tracking/190822/20190822_112842909_2BEE_generated_20210503_074806_nn.csv', newline='') as csvfile:
-         traces = parse_traces(csvfile)
-         ## INDEPENDENT TRACE-LIKE ANALYSIS
-         for index, trace in enumerate(traces.keys()):
-            print()
-            spam = trace_len(traces[trace])
-            print("Agent nummber", index, ". Number_of_frames, frame_range_len, trace_lenn, max_step_len, max_step_len_index, max_step_len_line:", spam)
-            if spam[2] == 0:
-                print("This trace has length of 0. Consider deleting this agent")
-            if spam[3] > bee_max_step_len:
-                print("This agent has moved", bee_max_step_len, "in a single step, you might consider deleting it.")
+        traces = parse_traces(csvfile)
+        traces_lenghts = []
+        for index, trace in enumerate(traces.keys()):
+           traces_lenghts.append(trace_len(traces[trace]))
 
+        ## INDEPENDENT TRACE-LIKE ANALYSIS
+        for index, trace_length in enumerate(traces_lenghts):
+           print("Agent number", index, ". Number_of_frames, frame_range_len, trace_lenn, max_step_len, max_step_len_index, max_step_len_line:", trace_length)
+           if trace_length[3] == 0:
+               print("This trace has length of 0. Consider deleting this agent")
+           if trace_length[4] > bee_max_step_len:
+               print("This agent has moved", bee_max_step_len, "in a single step, you might consider deleting it.")
 
-    # print(i)
+        ## CROSS-TRACE ANALYSIS
+        for index, trace in enumerate(traces_lenghts):
+            for index2, trace2 in enumerate(traces_lenghts):
+                if index == index2:
+                    continue
+                if abs(trace[0][1] - trace2[0][0]) < 100:
+                    # print(traces[index])
+                    # print(traces[index]["23325"])
+                    # print()
+                    print(traces[index][str(trace[0][1])][1])
+                    print(traces[index2][str(trace2[0][0])][1])
+                    point_distance = math.dist(list(map(float, (traces[index][str(trace[0][1])][1]))), list(map(float, (traces[index2][str(trace2[0][0])][1]))))
+                    message = "The beginning of trace", index2, "is close to end of trace", index, "by", abs(trace[0][1] - trace2[0][0]), "while the x,y distance is ", point_distance, "Consider joining them."
+
+                    if index2 == index + 1:
+                        if point_distance < 10:
+                            print(colored(message, "blue"))
+                        else:
+                            print(colored(message, "yellow"))
+                    else:
+                        print(message)
