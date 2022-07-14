03c760fb9c539096408635d1e3ae23c5ba3c8b24
Matej Hajnal <matej.hajnal@gmail.com>
2022-07-14 19:03:14 +0200

    INDEPENDENT TRACE-LIKE ANALYSIS, trace len updated

--- src/parse.py
+++ src/parse.py
@@ -2,6 +2,15 @@ import csv
 import math
 
 
+class Trace:
+    """ Single agent trace obtained from the loopy csv file or changed within the analysis.
+
+    """
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
 def dummy_colision_finder(csvfile, size):
     """ Parses  a loopy csv file nn/ai. It prints the frame numbers where the additional agents. The print includes agent's id.
         Returns a list of frames where an additional agent was found
@@ -40,7 +49,7 @@ def parse_traces(csvfile):
         if int(row['oid']) not in traces.keys():
             # print("hello", row['oid'])
             traces[int(row['oid'])] = dict()
-        traces[int(row['oid'])][row['frame_number']] = [row['x'], row['y']]
+        traces[int(row['oid'])][row['frame_number']] = [row[''], [row['x'], row['y']]]
     return traces
 
 
@@ -49,7 +58,7 @@ def trace_len(trace: dict):
 
     :param trace: a single trace of traces
 
-    :returns [number_of_frames, frame_range_len, trace_lenn]: [number of frames of the trace, [minimal, maximal frame number], length of the trace in x,y space]
+    :returns [number_of_frames, frame_range_len, trace_lenn, max_step_len]: [number of frames of the trace, [minimal, maximal frame number], length of the trace in x,y space, maximal length of a single step between the frames]
 
     """
     frames = sorted(list(map(int, trace.keys())))
@@ -59,6 +68,10 @@ def trace_len(trace: dict):
     frame_range = (frames[0], frames[-1])
     # print(frame_range)
     frame_range_len = float(frames[-1]) - float(frames[0])
+    max_step_len = 0
+    max_step_len_step_index = None
+    max_step_len_line = None
+    max_step_len_frame_number = None
 
     trace_lenn = 0
     for index, frame in enumerate(frames):
@@ -68,8 +81,15 @@ def trace_len(trace: dict):
             # print("index", index)
             # print("frames index ", frames[index])
             # print("traces frames index ", trace[str(frames[index])])
-            # print("map it to floats", list(map(float, (trace[str(frames[index])]))))
-            trace_lenn = trace_lenn + math.dist(list(map(float, (trace[str(frames[index])]))), list(map(float, (trace[str(frames[index+1])]))))
+            # print("traces frames index, x,y part", trace[str(frames[index])][1])
+            # print("map it to floats", list(map(float, (trace[str(frames[index])][1]))))
+            step_len = math.dist(list(map(float, (trace[str(frames[index])][1]))), list(map(float, (trace[str(frames[index+1])][1]))))
+            if step_len > max_step_len:  ## Set max step len
+                max_step_len = step_len
+                max_step_len_step_index = index
+                max_step_len_line = trace[str(frames[index])][0]
+                max_step_len_frame_number = frame
+            trace_lenn = trace_lenn + step_len
         except IndexError as err:
             if not index == len(frames) - 1:
                 # print(index)
@@ -78,11 +98,14 @@ def trace_len(trace: dict):
                 # print("Error:", str(err))
                 raise err
 
-    return number_of_frames, frame_range_len, trace_lenn
+    return number_of_frames, frame_range_len, trace_lenn, max_step_len, max_step_len_step_index, max_step_len_line, max_step_len_frame_number
 
 
 if __name__ == "__main__":
+    ## VARIABLE DECLARATION
+    bee_max_step_len = 1000
 
+    ## CODE
     i = 0
     with open('../data/Video_tracking/190822/20190822_112842909_2BEE_generated_20210503_074806_nn.csv', newline='') as csvfile:
         reader = csv.DictReader(csvfile)
@@ -95,10 +118,24 @@ if __name__ == "__main__":
         # dummy_colision_finder(csvfile, 2)
         pass
 
-    with open('../data/Video_tracking/190822/20190822_112842909_2BEE_generated_20210503_074806_nn.csv', newline='') as csvfile:
+    with open('../data/Video_tracking/190822/test.csv', newline='') as csvfile:
         traces = parse_traces(csvfile)
         # print()
         # print(traces[2])
+        print("number_of_frames, frame_range_len, trace_length:", trace_len(traces[0]))
         print("number_of_frames, frame_range_len, trace_length:", trace_len(traces[1]))
+
+    with open('../data/Video_tracking/190822/20190822_112842909_2BEE_generated_20210503_074806_nn.csv', newline='') as csvfile:
+         traces = parse_traces(csvfile)
+         ## INDEPENDENT TRACE-LIKE ANALYSIS
+         for index, trace in enumerate(traces.keys()):
+            print()
+            spam = trace_len(traces[trace])
+            print("Agent nummber", index, ". Number_of_frames, frame_range_len, trace_lenn, max_step_len, max_step_len_index, max_step_len_line:", spam)
+            if spam[2] == 0:
+                print("This trace has length of 0. Consider deleting this agent")
+            if spam[3] > bee_max_step_len:
+                print("This agent has moved", bee_max_step_len, "in a single step, you might consider deleting it.")
+
 
     # print(i)
