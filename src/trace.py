c6d9c9500b2a6bd35ec8e6641e7ee2857bea7588
xhajnal <matej.hajnal@gmail.com>
2022-07-20 17:22:24 +0200

    refactoring, Histogram of step lengths

---
+++ src/trace.py
@@ -0,0 +1,77 @@
+import math
+import matplotlib.pyplot as plt
+
+
+class Trace:
+    """ Single agent trace
+
+        Stores:
+        frame_range (tuple): a pair frame numbers, first and last
+        number_of_frames (int): number of frames
+        frame_range_len (int): length of trace in frames
+        trace_lenn (float): length of trace in x,y
+        max_step_len (float): maximal length of a single step in x,y
+        max_step_len_step_index (int): index of the longest step in x,y
+        max_step_len_line (int): line of .csv file, where the longest step occurred
+        max_step_len_frame_number (int): frame number, where the longest step occurred
+    """
+
+    def __init__(self, trace, id):
+        """ Parses a single agent trace obtained from the loopy csv file.
+
+            :param trace: a single trace of traces
+            :param id (int): id of the trace
+        """
+        self.id = id
+
+        frames = sorted(list(map(int, trace.keys())))
+        # print("frames", frames)
+
+        self.number_of_frames = len(trace.keys())
+        self.frame_range = (frames[0], frames[-1])
+        # print(frame_range)
+        self.frame_range_len = float(frames[-1]) - float(frames[0])
+        self.max_step_len = 0
+        self.max_step_len_step_index = None
+        self.max_step_len_line = None
+        self.max_step_len_frame_number = None
+
+        self.trace_lenghts = dict()
+
+        self.trace_lenn = 0
+        for index, frame in enumerate(frames):
+            # print(trace[frames[index]])
+            # print(trace[frames[index+1]])
+            try:
+                # print("index", index)
+                # print("frames index ", frames[index])
+                # print("traces frames index ", trace[str(frames[index])])
+                # print("traces frames index, x,y part", trace[str(frames[index])][1])
+                # print("map it to floats", list(map(float, (trace[str(frames[index])][1]))))
+                step_len = math.dist(list(map(float, (trace[str(frames[index])][1]))),
+                                     list(map(float, (trace[str(frames[index + 1])][1]))))
+                if step_len in self.trace_lenghts.keys():  ## count the number of lenghts
+                    self.trace_lenghts[step_len] = self.trace_lenghts[step_len] + 1
+                else:
+                    self.trace_lenghts[step_len] = 1
+                if step_len > self.max_step_len:  ## Set max step len
+                    self.max_step_len = step_len
+                    self.max_step_len_step_index = index
+                    self.max_step_len_line = int(trace[str(frames[index])][0]) + 2
+                    self.max_step_len_frame_number = frame
+                self.trace_lenn = self.trace_lenn + step_len
+            except IndexError as err:
+                if not index == len(frames) - 1:
+                    # print(index)
+                    # print(len(frames))
+                    # print(trace)
+                    # print("Error:", str(err))
+                    raise err
+
+    def show_step_lenghts_hist(self):
+        # print(self.trace_lenghts)
+        plt.bar(list(self.trace_lenghts.keys()), self.trace_lenghts.values(), color='g', )
+        plt.xlabel('Count of steps')
+        plt.ylabel('Step size')
+        plt.title(f'Histogram of step lengths. Trace {self.id}.')
+        plt.show()
--- test/test.py
+++ test/test.py
@@ -4,7 +4,7 @@ with open('../test/test.csv', newline='') as csvfile:
     traces = parse_traces(csvfile)
     # print()
     print(traces[0])
-    spam0 = Trace(traces[0])
+    spam0 = Trace(traces[0], 0)
     assert spam0.frame_range == (1620, 1622)
     assert spam0.number_of_frames == 3
     assert spam0.frame_range_len == 2
@@ -14,7 +14,7 @@ with open('../test/test.csv', newline='') as csvfile:
     assert spam0.max_step_len_line == 5
     assert spam0.max_step_len_frame_number == 1621
 
-    spam1 = Trace(traces[1])
+    spam1 = Trace(traces[1], 1)
     assert spam1.frame_range == (1620, 1622)
     assert spam1.number_of_frames == 3
     assert spam1.frame_range_len == 2
@@ -23,3 +23,5 @@ with open('../test/test.csv', newline='') as csvfile:
     assert spam1.max_step_len_step_index == 0
     assert spam1.max_step_len_line == 3
     assert spam1.max_step_len_frame_number == 1620
+
+    spam0.show_step_lenghts_hist()
