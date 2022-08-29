b77489bd23129172cf7ad56c9af4813fc667f8eb
Matej Hajnal <matej.hajnal@gmail.com>
2022-08-29 19:34:46 +0200

    update results

--- src/dave.py
+++ src/dave.py
@@ -5,38 +5,37 @@ if __name__ == "__main__":
     ## SINGLE BEE xxxx22
     # already done
     analyse('../data/Video_tracking/190822/20190822_111607344_1BEE_generated_20210430_080914_nn.csv', 1)
-    # 155 -> 11
+    # 156 -> 87 -> 11 -> 1
     analyse('../data/Video_tracking/190822/20190822_141925574_1bee_generated_20210504_081658_nn.csv', 1)
 
     ## xxxx23
-    # 14 -> 1
-    # done
+    # 15 -> 9 -> 1 done
     analyse('../data/Video_tracking/190823/20190823_114450691_1BEE_generated_20210506_100518_nn.csv', 1)
-    # 5 -> 1 done
+    # 6 -> 3 -> 1 done
     analyse('../data/Video_tracking/190823/20190823_153007029_1BEE_generated_20210507_091854_nn.csv', 1)
 
     ## TWO BEES xxxx22
-    # 64 -> 34
+    # 65 -> 56 -> 34
     analyse('../data/Video_tracking/190822/20190822_112842909_2BEE_generated_20210503_074806_nn.csv', 2)
-    # 78 -> 28
+    # 79 -> 70 -> 28
     analyse('../data/Video_tracking/190822/20190822_143216366_2bees_generated_20210504_064410_nn.csv', 2)
 
     ## xxxx23
-    # 170 -> 3
+    # 171 -> 122 (outside arena) -> 66  -> 3
     analyse('../data/Video_tracking/190823/20190823_115857275_2BEES_generated_20210507_083510_nn.csv', 2)
-    # 54 -> 9
+    # 55 -> 53 (outside arena) -> 46 -> 9
     analyse('../data/Video_tracking/190823/20190823_154249666_2BEES_generated_20210510_095112_nn.csv', 2)
 
     ## FIVE BEES xxxx22
-    # 1415 -> 1091 -> 743
+    # 1416 -> 1091 -> 743
     analyse('../data/Video_tracking/190822/20190822_114441236_5BEE_generated_20210503_090128_nn.csv', 5)
-    # 525 -> 415 -> 117
+    # 526 -> 415 -> 117
     analyse('../data/Video_tracking/190822/20190822_144547243_5BEE_generated_20210504_081238_nn.csv', 5)
 
     ## xxxx23
-    # 987 -> 50
+    # 987 -> 791 -> 50
     analyse('../data/Video_tracking/190823/20190823_121326323_5BEES_generated_20210505_103301_nn.csv', 5)
-    # 393 -> 256
+    # 393 -> 279 -> 256
     analyse('../data/Video_tracking/190823/20190823_155506355_5BEES_generated_20210507_092606_nn.csv', 5)
 
     ## SEVEN BEES xxxx22
@@ -44,7 +43,7 @@ if __name__ == "__main__":
     analyse('../data/Video_tracking/190822/20190822_115819107_7BEE_generated_20210504_064122_nn.csv', 7)
 
     ## xxxx23
-    # 477 -> 372 -> 128
+    # 477 -> 473 (outside arena) -> 372 -> 128
     analyse('../data/Video_tracking/190823/20190823_124111790_7BEES_generated_20210507_070601_nn.csv', 7)
     # 895 -> 638 -> 188
     analyse('../data/Video_tracking/190823/20190823_161115188_7BEES_generated_20210507_093529_nn.csv', 7)
@@ -64,5 +63,5 @@ if __name__ == "__main__":
     analyse('../data/Video_tracking/190822/20190822_122407809_15BEE_generated_20210803_085008_nn.csv', 15)
 
     ## xxxx23
-    # 2190 -> 1938 -> 
+    # 2190 -> 1938 ->
     analyse('../data/Video_tracking/190823/20190823_163934743_15BEES_generated_20210510_082044_nn.csv', 15)
