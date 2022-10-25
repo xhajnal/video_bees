d595377396e75c75b8fad65b9cb15eec91be5231
xhajnal <matej.hajnal@gmail.com>
2022-10-25 16:03:43 +0200

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,43 +1,55 @@
 
 def get_distance_from_calculated_arena():
     """ Returns distance from the estimated arena circle to be assigned as outside of the arena."""
-    return 50
+    # Usage: outside of arena
+    return 53
 
 
 def get_max_trace_gap():
     """ Returns maximal distance between two traces in frames to be able to merge."""
-    # TODO check this
-    return 500
+    # Usage: gaping trace - ABOVE
+    return 150
 
 
 def get_min_trace_length():
-    """ Returns a minimal distance of a trace to be merged with the other."""
+    """ Returns a minimal length of a trace to be merged with the other."""
     # TODO FIND THIS
-    return 1
+    # Usage: gaping traces
+    return 10
 
 
 def get_bee_max_step_len():
     """ Returns maximal step of a bee in a single frame in xy distance."""
     # TODO check this
-    return 150
+    # Usage: gaping traces - LONG gap
+    return 50
 
 
 def get_bee_max_step_len_per_frame():
     """ Returns maximal step of a bee in a single frame in xy distance in continuous manner over more than 50 frames."""
     # TODO check this
-    return 20
+    # Usage: gaping traces - SHORT gap
+    return 28
+
+
+def get_max_trace_gap_to_interpolate_distance():
+    """ Returns a maximal distance in frames so that the location of the bee is linearly interpolated based on border points. """
+    # TODO check this
+    # Usage: gaping traces, whether to calculate the location in the gap
+    return 100  ## 1 second (as frame rate is 100)
 
 
 def get_max_step_distance_to_merge_overlapping_traces():
     """ Returns a maximal distance two traces can differ in any point to be not merged."""
     # TODO check this
+    # Usage: overlapping traces
     return 200
 
 
-def get_max_trace_gap_to_interpolate_distance():
-    """ Returns a maximal distance so that the location of the bee is linearly interpolated based on border points"""
+def get_maximal_distance_to_check_for_trace_swapping():
+    """ Returns a maximal distance in xy of two traces at a certain frame to check whether the traces are not swapped."""
     # TODO check this
-    return 200
+    return 100
 
 
 def get_screen_size():
