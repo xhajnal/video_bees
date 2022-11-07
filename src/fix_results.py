b98110ca793ba4260b09fe4ac4fdce1e7ab29850
xhajnal <matej.hajnal@gmail.com>
2022-11-07 16:49:24 +0100

    Fixes order of records. Trim parts from record.

--- src/fix_results.py
+++ src/fix_results.py
@@ -2,9 +2,12 @@ import json
 from time import time
 from termcolor import colored
 
+from dave_io import convert_results_from_json_to_csv
+
 
 def check_setting():
-    """
+    """ Fixes population size column,
+
     """
     print(colored("SAVE SETTING AND COUNTS OF TRACES AS JSON", "blue"))
     start_time = time()
@@ -37,5 +40,62 @@ def check_setting():
         file.write(json.dumps(results))
 
 
+def fix_order_setting():
+    """ Fixes order of records. Trim parts from record."""
+    print(colored("SAVE SETTING AND COUNTS OF TRACES AS JSON", "blue"))
+    start_time = time()
+
+    ## LOAD SAVED RESULTS TO UPDATE IT
+    with open("../output/results.txt") as file:
+        results = json.load(file)
+
+    results1 = {}
+    results2 = {}
+    results5 = {}
+    results7 = {}
+    results10 = {}
+    results15 = {}
+
+    for file in results.keys():
+        # trim part from the record
+        if "part" in str(file).lower():
+            continue
+        if "_1bee" in str(file).lower():
+            # print("hello")
+            results1[file] = results[file]
+        elif "_2bee" in str(file).lower():
+            # print("hello")
+            results2[file] = results[file]
+        elif "_5bee" in str(file).lower():
+            # print("hello")
+            results5[file] = results[file]
+        elif "_7bee" in str(file).lower():
+            # print("hello")
+            results7[file] = results[file]
+        elif "_10bee" in str(file).lower():
+            # print("hello")
+            results10[file] = results[file]
+        elif "_15bee" in str(file).lower():
+            # print("hello")
+            results15[file] = results[file]
+
+    for file in results2.keys():
+        results1[file] = results2[file]
+    for file in results5.keys():
+        results1[file] = results5[file]
+    for file in results7.keys():
+        results1[file] = results7[file]
+    for file in results10.keys():
+        results1[file] = results10[file]
+    for file in results15.keys():
+        results1[file] = results15[file]
+
+    with open("../output/results.txt", 'w') as file:
+        file.write(json.dumps(results1))
+
+    convert_results_from_json_to_csv()
+
+
 if __name__ == "__main__":
-    check_setting()
+    # check_setting()
+    fix_order_setting()
