2adb02fc7db9f75ecd56240df2056f80488d505f
xhajnal <matej.hajnal@gmail.com>
2022-12-20 19:13:15 +0100

    Update show_aligment.py

--- src/help/show_aligment.py
+++ src/help/show_aligment.py
@@ -5,7 +5,6 @@ from analyse import analyse
 
 if __name__ == "__main__":
     os.chdir('..')
-    ## 190822
-    # 1416 -> 1091 -> 679
-    # GOT VIDEO
+    analyse('../data/Video_tracking/190822/20190822_111607344_1BEE_generated_20210430_080914_nn.csv', 1)
+    # raise Exception
     analyse("../data/Video_tracking/190822/20190822_114441236_5BEE_generated_20210503_090128_nn.csv", 5)
