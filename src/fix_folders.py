1667f92096f02d7247ae638cd5bdc5f7bb15df27
xhajnal <matej.hajnal@gmail.com>
2023-01-11 04:38:41 +0100

    Create fix_folders.py

---
+++ src/fix_folders.py
@@ -0,0 +1,29 @@
+import glob
+import os
+import shutil
+
+os.chdir("../data/Video_tracking")
+
+## for each subfolder (day of tracking)
+for folder in next(os.walk('.'))[1]:
+    print(folder)
+    # make a folder original inside
+    try:
+        os.mkdir(f"{folder}/original")
+    except:
+        pass
+
+    os.chdir(folder)
+
+    # move all files from the loopy folder one up
+    for file in glob.glob(f"deep*/*"):
+        print("file", file)
+        shutil.move(file, f"{os.path.basename(file)}")
+
+    # move all the csv files to folder original
+    for file in glob.glob(f"*.csv"):
+        print("file", os.path.basename(file))
+        shutil.move(f"{os.path.basename(file)}", f"original/{os.path.basename(file)}")
+
+    os.chdir("..")
+    break
