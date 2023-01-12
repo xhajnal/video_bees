a3f48994e4b8b0b5ca8e48e8c4752d029c2ff6c0
xhajnal <matej.hajnal@gmail.com>
2023-01-12 15:34:42 +0100

    fix examples config - updated to its normal version

--- src/help/show_two_runs.py
+++ src/help/show_two_runs.py
@@ -1,7 +1,5 @@
 from analyse import analyse, set_just_annotate, set_force_new_video
 import os
-import config
-from analyse import analyse
 
 
 def run_both():
@@ -23,11 +21,10 @@ def run(is_first_run=None):
     analyse("../data/Video_tracking/190903/20190903_134034775_1BEE_generated_20210511_083234_nn.csv", 1, is_first_run=is_first_run)
 
     # 62 -> 29 -> 1 Done, video checked, passed
-    analyse("../data/Video_tracking/190903/20190903_172942424_1BEE_generated_20210520_073610_nn.csv", 1,
-            has_tracked_video=True, is_first_run=is_first_run)
+    analyse("../data/Video_tracking/190903/20190903_172942424_1BEE_generated_20210520_073610_nn.csv", 1, has_tracked_video=True, is_first_run=is_first_run)
 
 
 if __name__ == "__main__":
     os.chdir('..')
-    # run_both()
-    run_just_annotate()
+    run_both()
+    # run_just_annotate()
