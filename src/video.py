d3b1417fd3fbced83158bfcfe44e66086638d0b1
Matej Hajnal <matej.hajnal@gmail.com>
2023-01-18 00:45:13 +0100

    video weird hotfix buttons

--- src/video.py
+++ src/video.py
@@ -335,15 +335,24 @@ def annotate_video(input_video, output_video, traces, frame_range, speed=1, trac
                         video.set(cv2.CAP_PROP_POS_FRAMES, trim_offset + frame_range[0])
                     else:
                         video.set(cv2.CAP_PROP_POS_FRAMES, trim_offset)
-                    locations_of_traces = [[]]*len(traces)
+                    # locations_of_traces = [[]]*len(traces)
+                    locations_of_traces = []
+                    for trace in traces:
+                        locations_of_traces.append([])
 
                 if key == ord('a') or key == ord('A'):
                     video.set(cv2.CAP_PROP_POS_FRAMES, max(trim_offset + frame_number - 100, trim_offset + frame_range[0]))
-                    locations_of_traces = [[]]*len(traces)
+                    # locations_of_traces = [[]]*len(traces)
+                    locations_of_traces = []
+                    for trace in traces:
+                        locations_of_traces.append([])
 
                 if key == ord('d') or key == ord('D'):
                     video.set(cv2.CAP_PROP_POS_FRAMES, min(trim_offset + frame_number + 100, trim_offset + frame_range[1]))
-                    locations_of_traces = [[]]*len(traces)
+                    # locations_of_traces = [[]]*len(traces)
+                    locations_of_traces = []
+                    for trace in traces:
+                        locations_of_traces.append([])
 
                 if key == ord('+'):
                     speed = 1.1 * speed
